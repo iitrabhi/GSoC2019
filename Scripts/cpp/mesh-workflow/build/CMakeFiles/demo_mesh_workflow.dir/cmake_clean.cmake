a1f8f07b859fd7ddd60f8ae0d12dcb6bcd0b61b2
Abhinav Gupta <iitrabhi@gmail.com>
2019-06-03 10:55:37 +0530

    add cpp mesh workflow

---
+++ Scripts/cpp/mesh-workflow/build/CMakeFiles/demo_mesh_workflow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo_mesh_workflow.dir/main.cpp.o"
+  "demo_mesh_workflow.pdb"
+  "demo_mesh_workflow"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo_mesh_workflow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
