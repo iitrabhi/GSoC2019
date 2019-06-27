9b583af756fc0d103584800601a6700c253f4ff4
Abhinav Gupta <iitrabhi@gmail.com>
2019-06-27 20:07:23 +0530

    Update Ci config

--- Scripts/cpp/mesh-workflow-new/main.cpp
+++ Scripts/cpp/mesh-workflow-new/main.cpp
@@ -8,14 +8,14 @@ int main(int argc, char* argv[])
   common::SubSystemsManager::init_logging(argc, argv);
   common::SubSystemsManager::init_petsc(argc, argv);
 
-  //dolfin::io::XDMFFile xdmf_file(MPI_COMM_WORLD,"../input/unit_square.xdmf");
-  dolfin::io::XDMFFile xdmf_file(MPI_COMM_WORLD,"../input/mesh_2d.xdmf");
+  dolfin::io::XDMFFile xdmf_file(MPI_COMM_WORLD,"../input/unit_square.xdmf");
+  //dolfin::io::XDMFFile xdmf_file(MPI_COMM_WORLD,"../input/mesh_2d.xdmf");
  
 
   auto mesh_2d = std::make_shared<mesh::Mesh>(xdmf_file.read_mesh(MPI_COMM_WORLD,mesh::GhostMode::none));
 
  
-  std::cout<<mesh_2d->topology().str(true);
+  //std::cout<<mesh_2d->topology().str(true);
 
   auto mapOfTag = xdmf_file.read_tags();
 
@@ -39,8 +39,8 @@ int main(int argc, char* argv[])
   }
   //std::cout<<"Hmin: "<< mesh_2d->hmin() <<std::endl;
 //
-  dolfin::io::XDMFFile xdmf_file_mvc(MPI_COMM_WORLD,"../input/mvc_1d.xdmf");
-  auto mvc = xdmf_file_mvc.read_mvc_int(mesh_2d,"name_to_read");
+  //dolfin::io::XDMFFile xdmf_file_mvc(MPI_COMM_WORLD,"../input/mvc_1d.xdmf");
+  //auto mvc = xdmf_file_mvc.read_mvc_int(mesh_2d,"name_to_read");
 //
   //std::cout<<"MVC size: "<< mvc.size() <<std::endl;
   //auto mf = dolfin::mesh::MeshFunction<int>(mesh_2d,mvc,1);
