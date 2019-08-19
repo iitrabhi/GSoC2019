531ab5723e36350fefd8fb7a48cb4cd2eda40c3f
Abhinav Gupta <iitrabhi@gmail.com>
2019-08-19 14:09:37 +0530

    Add code to make 3D mesh in cpp

--- Scripts/cpp/mesh-workflow/main.cpp
+++ Scripts/cpp/mesh-workflow/main.cpp
@@ -8,30 +8,55 @@ int main(int argc, char* argv[])
   common::SubSystemsManager::init_logging(argc, argv);
   common::SubSystemsManager::init_petsc(argc, argv);
 
-  dolfin::io::XDMFFile xdmf_file(MPI_COMM_WORLD,"../input/mesh_2d.xdmf");
-  auto mesh_2d = std::make_shared<mesh::Mesh>(xdmf_file.read_mesh(MPI_COMM_WORLD,mesh::GhostMode::none));
+  // dolfin::io::XDMFFile xdmf_file(MPI_COMM_WORLD,"../input/mesh_2d.xdmf");
+  // auto mesh_2d = std::make_shared<mesh::Mesh>(xdmf_file.read_mesh(mesh::GhostMode::none));
   
-  //std::cout<< mesh_2d->hmin() <<std::endl;
+  // std::cout<< mesh_2d->hmin() <<std::endl;
 
-  dolfin::io::XDMFFile xdmf_file_mvc(MPI_COMM_WORLD,"../input/mvc_1d.xdmf");
-  auto mvc = xdmf_file_mvc.read_mvc_int(mesh_2d,"name_to_read");
+  // Create mesh and function space
+  std::array<Eigen::Vector3d, 2> pt{Eigen::Vector3d(0.0, 0.0, 0.0),
+                                    Eigen::Vector3d(1.0, 1.0, 1.0)};
+  auto _mesh = std::make_shared<mesh::Mesh>(generation::BoxMesh::create(
+      MPI_COMM_WORLD, pt, {{32, 32, 32}}, mesh::CellType::hexahedron,
+      mesh::GhostMode::none));
+  
+  auto entity_index = 0;
+  auto _dim = 2;
+  const std::size_t D = _mesh->topology().dim();
 
-  //std::cout<< mvc.size() <<std::endl;
+  _mesh->create_entities(_dim);
 
-  auto mf = dolfin::mesh::MeshFunction<int>(mesh_2d,mvc,1);
-  auto val = mf.values();
+  _mesh->create_connectivity(_dim, D);
+  assert(_mesh->topology().connectivity(_dim, D));
+  const mesh::Connectivity& connectivity = *_mesh->topology().connectivity(_dim, D);
 
-  std::cout<<"Dimension: "<< mf.dim() <<std::endl;
-  std::cout<<"Size: "<< mf.size() <<std::endl;
 
-  std::cout<< "Values" <<std::endl;
-  for(int i=0;i<mf.size();i++){
-      if(i%20==0){
-          std::cout<<std::endl;
-      }else{
-          std::cout<< val[i] <<"    ";
-      }
-  }
-  std::cout<<std::endl;
+  const mesh::MeshEntity entity(*_mesh, _dim, entity_index);
+
+  dolfin::mesh::CellType type = _mesh->cell_type;
+  const std::int32_t num_vertices_per_cell = mesh::num_cell_vertices(type);
+
+  std::cout<< num_vertices_per_cell <<std::endl;
+
+  // dolfin::io::XDMFFile xdmf_file_mvc(MPI_COMM_WORLD,"../input/mvc_1d.xdmf");
+  // auto mvc = xdmf_file_mvc.read_mvc_int(mesh_2d,"name_to_read");
+// 
+  // //std::cout<< mvc.size() <<std::endl;
+// 
+  // auto mf = dolfin::mesh::MeshFunction<int>(mesh_2d,mvc,1);
+  // auto val = mf.values();
+// 
+  // std::cout<<"Dimension: "<< mf.dim() <<std::endl;
+  // std::cout<<"Size: "<< mf.size() <<std::endl;
+// 
+  // std::cout<< "Values" <<std::endl;
+  // for(int i=0;i<mf.size();i++){
+  //     if(i%20==0){
+  //         std::cout<<std::endl;
+  //     }else{
+  //         std::cout<< val[i] <<"    ";
+  //     }
+  // }
+  // std::cout<<std::endl;
   return 0;
 }
---
+++ Scripts/python/mvc-array/test_mvc_array.py
@@ -0,0 +1,35 @@
+import pygmsh
+from dolfin import MPI, MeshValueCollection, cpp
+
+geom = pygmsh.opencascade.Geometry()
+
+mesh_ele_size = 0.5
+p0 = geom.add_point([0, 0, 0], lcar=mesh_ele_size)
+p1 = geom.add_point([1, 0, 0], lcar=mesh_ele_size)
+p2 = geom.add_point([1, 1, 0], lcar=mesh_ele_size)
+p3 = geom.add_point([0, 1, 0], lcar=mesh_ele_size)
+
+l0 = geom.add_line(p0, p1)
+l1 = geom.add_line(p1, p2)
+l2 = geom.add_line(p2, p3)
+l3 = geom.add_line(p3, p0)
+
+ll = geom.add_line_loop(lines=[l0, l1, l2, l3])
+ps = geom.add_plane_surface(ll)
+
+# Tag line and surface
+geom.add_physical(l3, label="LINE")
+geom.add_physical(ps, label="SURFACE")
+
+pygmsh_mesh = pygmsh.generate_mesh(geom)
+points, cells, cell_data = pygmsh_mesh.points, pygmsh_mesh.cells, pygmsh_mesh.cell_data
+
+mesh = cpp.mesh.Mesh(MPI.comm_world,
+                     cpp.mesh.CellType.triangle, points,
+                     cells['triangle'], [], cpp.mesh.GhostMode.none)
+assert mesh.degree() == 1
+assert mesh.geometry.dim == 3
+assert mesh.topology.dim == 2
+
+f = MeshValueCollection("size_t", mesh, 1, cells["line"], cell_data["line"]['gmsh:physical'])
+assert f.get_value(1, 1) == 1
